e30d04a706b275dc83773f4adb8d51a4d762328d
wocoburguesa <wocoburguesa@gmail.com>
2016-10-08 18:41:12 -0500

    Added search

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1,24 +1,60 @@
 (function () {
     'use strict';
     angular.module('cognitoolkit')
-        .controller('ModelsCtrl', function (models, frequencyMap, topics, text) {
+        .controller('ModelsCtrl', function (models, frequencyMap, topics, /*text,*/ searchIndex) {
             var $ctrl = this;
+            var searchMap;
 
-            text.success(function (data) {
+            var urlMap = {};
+
+            function generateWordIndex(wordStats) {
+                var result = {};
+
+                for (var i = 0; i < 10000; i++) {
+                    result[wordStats[i].word] = wordStats[i].stats.appearsIn;
+                }
+
+                return result;
+            }
+
+            function searchSingleWord(word) {
+                var upper = word.toUpperCase();
+
+                if (searchMap[upper]) {
+                    for (var i = 0; i < searchMap[upper].length; i++) {
+                        if ($ctrl.models[searchMap[upper][i]]) {
+                            $ctrl.models[searchMap[upper][i]].hide = false;
+                        }
+                    }
+                }
+            }
+
+/*            text.success(function (data) {
                 window.data = data;
+                window.totalCount = 0;
                 var words = {};
                 var sortable = [];
-                var desc;
+                var desc, url, model;
 
                 for (var i = 0; i < data.length; i++) {
                     desc = data[i]['New column'].split(' ');
+                    url = data[i]['url'];
+                    model = urlMap[url];
 
                     for (var j = 0; j < desc.length; j++) {
+                        window.totalCount++;
                         if (desc[j].length > 4) {
                             if (words[desc[j]]) {
-                                words[desc[j]]++;
+                                words[desc[j]].count++;
+
+                                if (words[desc[j]].appearsIn.indexOf(model) === -1) {
+                                    words[desc[j]].appearsIn.push(model);
+                                }
                             } else {
-                                words[desc[j]] = 1;
+                                words[desc[j]] = {
+                                    count: 1,
+                                    appearsIn: [model]
+                                };
                             }
                         }
                     }
@@ -27,13 +63,24 @@
                 for (var key in words) {
                     sortable.push({
                         word: key,
-                        count: words[key]
+                        stats: words[key]
                     });
                 }
 
                 window.data2 = sortable.sort(function (a, b) {
-                    return b.count - a.count;
+                    return b.stats.count - a.stats.count;
                 });
+
+                window.data3 = generateWordIndex(window.data2);
+            });
+*/
+
+            searchIndex.success(function (data) {
+                searchMap = data;
+
+                for (var key in data) {
+                    data[key.toUpperCase()] = data[key];
+                }
             });
 
             $ctrl.currentTopic = '';
@@ -60,8 +107,25 @@
                     (model.tema == $ctrl.currentTopic || !$ctrl.currentTopic);
             };
 
+            $ctrl.search = function () {
+                var words = $ctrl.searchQuery.split(' ');
+
+                if (!$ctrl.searchQuery || !searchMap) {
+                    for (var i = 0; i < $ctrl.models.length; i++) {
+                        $ctrl.models[i].hide = false;
+                    }
+                    return;
+                }
+
+                for (var i = 0; i < $ctrl.models.length; i++) {
+                    $ctrl.models[i].hide = true;
+                }
+                for (var i = 0; i < words.length; i++) {
+                    searchSingleWord(words[i]);
+                }
+            };
+
             $("#jumpTo").on("click", function(e) {
-                console.log(e);
                 e.preventDefault();
                 $("html, body").animate({
                     scrollTop: $('#searchSection').offset().top
@@ -69,8 +133,11 @@
             });
 
             models.success(function (data) {
-                console.log('ASI SE VE MODELS AL FINAL:', data);
                 $ctrl.models = data;
+
+                for (var i = 0; i < data.length; i++) {
+                    urlMap[data[i].enlace] = i;
+                }
             });
       });
 })();
--- scripts/services.js
+++ scripts/services.js
@@ -4,7 +4,10 @@
         .service('models', function ($http) {
             return $http.get('models.json');
         })
-        .service('text', function ($http) {
+/*        .service('text', function ($http) {
             return $http.get('text.csv');
+        })*/
+        .service('searchIndex', function ($http) {
+            return $http.get('search-index.json');
         });
 })();
